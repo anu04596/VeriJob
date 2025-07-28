6f46b3e1fe3335a6e75c13366c9cf6f00835e63a
anu04596 <142776325+anu04596@users.noreply.github.com>
2025-07-28 12:32:16 +0530

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
+import API_BASE_URL from './config';
 
 const Dashboard = () => {
   const navigate = useNavigate();
@@ -30,7 +31,7 @@ const Dashboard = () => {
       return;
     }
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("${API_BASE_URL}/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json",
           "Authorization": `Bearer ${token}`,
